45cf95486ebc8802f62e14dbe05fd45e23be9527
xuliya <xuliya@xuliyas-mbp.cn.ibm.com>
2019-03-20 10:30:24 +0800

    Add a new job in building stemcell pipeline to OS reload an existing VM with the newly created SL community stemcell

---
+++ ci/softlayer/tasks/deploy-vm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e -x -u
+
+function check_param() {
+  local name=$1
+  local value=$(eval echo '$'$name)
+  if [ "$value" == 'replace-me' ]; then
+    echo -e "\n[ERROR] environment variable $name must be set"
+    exit 1
+  fi
+}
+
+check_param PUBLISHED_BUCKET_NAME
+check_param OS_NAME
+check_param OS_VERSION
+
+export TASK_DIR=$PWD
+export VERSION=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
+
+echo "[INFO] Install dependencies packages"
+apt-get update
+apt-get install curl -y
+
+tar zxvf stemcell/*.tgz  -C ./
+image_id=`grep "virtual-disk-image-id:" stemcell.MF| cut -d ":" -f2 | sed 's/^[ \t]*//g' `
+
+result_str=`curl --silent -X POST -d '{"parameters":[ "FORCE",  { "imageTemplateId": "'$image_id'" }] }' https://$SL_USERNAME:$SL_API_KEY@api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest/$VM_ID/reloadOperatingSystem.json`
+result=`echo $result_str |sed 's/\"//g'`
+if [ "$result" != "1" ] ; then
+   echo "Reload vm failed"
+   exit 1
+else
+   echo "Reload vm done!"
+fi
+
+
+
