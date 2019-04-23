895d59c1ab4e5c4c496a27fe007715cb4c63829d
Bin Gu <gubin@cn.ibm.com>
2019-04-23 15:55:46 +0800

    add ready to do os-reload check

--- ci/softlayer/tasks/deploy-vm.sh
+++ ci/softlayer/tasks/deploy-vm.sh
@@ -25,17 +25,29 @@ apt-get install curl -y
 tar zxvf stemcell/*.tgz  -C ./
 image_id=`grep "virtual-disk-image-id:" stemcell.MF| cut -d ":" -f2 | sed 's/^[ \t]*//g' `
 sl_username=`echo $SL_USERNAME |sed 's/@/%40/g'`
-result_str=`curl --silent -X POST -d '{"parameters":[ "FORCE",  { "imageTemplateId": "'$image_id'" }] }' https://$sl_username:$SL_API_KEY@api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest/$VM_ID/reloadOperatingSystem.json`
-result=`echo $result_str |sed 's/\"//g'`
-if [ "$result" != "1" ] ; then
-   echo "Reload failed to launch!"
-   exit 1
-else
-   echo "Reload launched!"
-fi
 
 # get active transaction curl command
 curl_cmd="curl --silent -u $sl_username:$SL_API_KEY https://api.service.softlayer.com/rest/v3.1/SoftLayer_Virtual_Guest/$VM_ID.json?objectMask=mask%5Bid%2C+activeTransaction%5Bid%2CtransactionStatus.name%5D%5D"
+
+# check if the VM is ready for os-reload (no active transaction)
+response=`$curl_cmd`
+if [[ $response =~ "activeTransaction" ]]
+then
+    echo "There is active transactions running on the VM. Can't do OS relaod for now."
+    exit 1
+else
+    echo "The VM is ready to do OS reload. Launching OS relaod..."
+fi
+
+# do os-reload
+result_str=`curl --silent -X POST -d '{"parameters":[ "FORCE",  { "imageTemplateId": "'$image_id'" }] }' https://$sl_username:$SL_API_KEY@api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest/$VM_ID/reloadOperatingSystem.json`
+result=`echo $result_str |sed 's/\"//g'`
+if [ "$result" != "1" ] ; then
+   echo "OS reload failed to launch!"
+   exit 1
+else
+   echo "OS reload launched!"
+fi
 
 # check if os-reload starts in loop
 while true
