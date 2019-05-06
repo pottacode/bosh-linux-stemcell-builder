c58e7a84d87400c17e8943706310444b0c1b823d
Bin Gu <gubin@cn.ibm.com>
2019-05-06 15:23:15 +0800

    update to new bosh agent 2.215.3 which include "Configure dns servers for SL"

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -51,8 +51,8 @@ bosh_agent_version=$(cat ${assets_dir}/bosh-agent-version)
 if is_ppc64le; then
   /usr/bin/meta4 file-download --metalink=${assets_dir}/metalink.meta4 --file=bosh-agent-${bosh_agent_version}-linux-ppc64le bosh-agent
 else
-  curl_it "https://s3.amazonaws.com/ng-bosh-softlayer-agent/bosh-agent-2.215.3.1-sl-linux-amd64"
-  echo "e4c2ae6d9b001f522a9d05ea96310a4ddff8da2b  bosh-agent" | shasum -c -
+  curl_it "https://s3.amazonaws.com/ng-bosh-softlayer-agent/bosh-agent-2.215.3-sl-linux-amd64"
+  echo "89e529deded2d25ace268e85e9e89f635abcde53  bosh-agent" | shasum -c -
 fi
 
 mv bosh-agent $chroot/var/vcap/bosh/bin/
