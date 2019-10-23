68c81be00a58d793e47d9e7877b6750687ee0025
Bin Gu <gubin@cn.ibm.com>
2019-10-23 11:17:27 +0800

    update bosh agent to 2.234.3

--- stemcell_builder/stages/bosh_go_agent/apply.sh
+++ stemcell_builder/stages/bosh_go_agent/apply.sh
@@ -51,8 +51,8 @@ bosh_agent_version=$(cat ${assets_dir}/bosh-agent-version)
 if is_ppc64le; then
   /usr/bin/meta4 file-download --metalink=${assets_dir}/metalink.meta4 --file=bosh-agent-${bosh_agent_version}-linux-ppc64le bosh-agent
 else
-  curl_it "https://s3.amazonaws.com/ng-bosh-softlayer-agent/bosh-agent-2.234.2-sl-linux-amd64"
-  echo "48d013cf194d154308e805316f9e34fb762e48b4  bosh-agent" | shasum -c -
+  curl_it "https://s3.amazonaws.com/ng-bosh-softlayer-agent/bosh-agent-2.234.3-sl-linux-amd64"
+  echo "52211a0832379446db95ce259a10bab531f81741  bosh-agent" | shasum -c -
 fi
 
 mv bosh-agent $chroot/var/vcap/bosh/bin/
