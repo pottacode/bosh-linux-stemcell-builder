41eeb20eb70353499f193cdb532d92f8f2007fba
gu-bin <gubin@cn.ibm.com>
2020-03-19 16:53:01 +0800

    add backup-stemcell job

--- ci/custom_stemcell/tasks/backup-stemcell-images.sh
+++ ci/custom_stemcell/tasks/backup-stemcell-images.sh
@@ -13,28 +13,37 @@ api_key = ${SL_API_KEY}
 endpoint_url = https://api.softlayer.com/xmlrpc/v3.1/
 timeout = 0
 EOF
-#echo "nameserver 114.114.114.114" > /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-custom_stemcell_version=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
-pwd=`slcli vs detail --passwords ${stemcell_vm_id} | grep "users" | awk -F ' ' '{print $4}' `
+# get /var/vcap/bosh/etc/stemcell_version in the target VM
+root_pwd=`slcli vs detail --passwords ${stemcell_vm_id} | grep users | awk -F ' ' '{print $4}'`
 private_ip=`slcli vs detail --passwords ${stemcell_vm_id} | grep "private_ip" | awk -F ' ' '{print $2}'`
-sshpass -p ${pwd} ssh -o StrictHostKeychecking=no root@${private_ip} "echo '${custom_stemcell_version}'>/var/vcap/bosh/etc/stemcell_version"
-sshpass -p ${pwd} ssh -o StrictHostKeychecking=no root@${private_ip} "cat /var/vcap/bosh/etc/stemcell_version"
-echo -e "Capture the VM ${stemcell_vm_id} to a private image"
-if [ `slcli image list | grep "Template created from imported bosh-stemcell-${custom_stemcell_version}-bluemix-esxi-ubuntu-trusty-go_agent.vhd" | wc -l` -gt 0 ]; then
-  echo -e "The image with name 'Template created from imported bosh-stemcell-${custom_stemcell_version}-bluemix-esxi-ubuntu-trusty-go_agent.vhd' already exists, exiting..."
+stemcell_version=`sshpass -p $root_pwd ssh -o StrictHostKeychecking=no root@$private_ip "cat /var/vcap/bosh/etc/stemcell_version"`
+echo "/var/vcap/bosh/etc/stemcell_version file on VM ${stemcell_vm_id} is $stemcell_version"
+
+private_image_name="private-image-backup-for-bosh-stemcell-${stemcell_version}"
+public_image_name="public-image-backup-for-bosh-stemcell-${stemcell_version}"
+
+echo -e "Capture the VM ${stemcell_vm_id} to a private image for backup"
+if [ `slcli image list | grep ${private_image_name} | wc -l` -gt 1 ]; then
+  echo -e "There are more than 1 images with name ${private_image_name} exist. Please check. Exiting..."
   exit 1
 fi
 
-slcli vs capture -n "Template created from imported bosh-stemcell-${custom_stemcell_version}-bluemix-esxi-ubuntu-trusty-go_agent.vhd" "${stemcell_vm_id}"
-if [ $? -ne 0 ]; then
-  echo -e "The image capture failed, exiting..."
-  exit 1
+if [ `slcli image list | grep ${private_image_name} | wc -l` -eq 1 ]; then
+  echo -e "There is already 1 image with name ${private_image_name} exists. Use it directly..."
+else
+  echo -e "There is no image with name ${private_image_name} exists. Capture it..."
+  slcli vs capture -n ${private_image_name} ${stemcell_vm_id}
+  if [ $? -ne 0 ]; then
+    echo -e "The image capture failed, exiting..."
+    exit 1
+  fi
+  sleep 60
 fi
-sleep 60
 
-private_image_id=`slcli image list --name "Template created from imported bosh-stemcell-${custom_stemcell_version}-bluemix-esxi-ubuntu-trusty-go_agent.vhd" | tail -f | cut -d " " -f 1`
+private_image_id=`slcli image list --name ${private_image_name} | tail -f | cut -d " " -f 1`
+echo "The backup private image id is ${private_image_id}"
 
 capture_success=false
 for (( i=1; i<=60; i++ ))
@@ -55,18 +64,17 @@ if [ "${capture_success}" = false ]; then
   exit 1
 fi
 
-private_image_id=`slcli image list --name "Template created from imported bosh-stemcell-${custom_stemcell_version}-bluemix-esxi-ubuntu-trusty-go_agent.vhd" | tail -f | cut -d " " -f 1`
-echo "The image private id is " ${private_image_id}
-echo -e "Convert the private image ${private_image_id} to a public image"
 sleep 120
+
+echo -e "Convert the backup private image ${private_image_id} to a public image"
 sl_username=`echo ${SL_USERNAME} |sed 's/@/%40/g'`
 
 curl -X POST -d "{
   \"parameters\":
   [
-    \"light-bosh-stemcell-${custom_stemcell_version}-bluemix-xen-ubuntu-trusty-go_agent\",
-    \"Public_light_stemcell_${custom_stemcell_version}\",
-    \"Public_light_stemcell_${custom_stemcell_version}\",
+    \"${public_image_name}\",
+    \"${public_image_name}\",
+    \"${public_image_name}\",
     [
       {
           \"id\":${sl_para_id},
@@ -75,10 +83,10 @@ curl -X POST -d "{
       }
     ]
   ]
-}" https://$sl_username:${SL_API_KEY}@api.softlayer.com/rest/v3.1/SoftLayer_Virtual_Guest_Block_Device_Template_Group/${private_image_id}/createPublicArchiveTransaction >> stemcell-image/stemcell-info-${custom_stemcell_version}.json
+}" https://$sl_username:${SL_API_KEY}@api.softlayer.com/rest/v3.1/SoftLayer_Virtual_Guest_Block_Device_Template_Group/${private_image_id}/createPublicArchiveTransaction >> stemcell-image/stemcell-info-${stemcell_version}.json
 
 sleep 20
-public_image_id=$(cat stemcell-image/stemcell-info-${custom_stemcell_version}.json | sed 's/\.0$//;s/\.0$//')
+public_image_id=$(cat stemcell-image/stemcell-info-${stemcell_version}.json | sed 's/\.0$//;s/\.0$//')
 convert_success=false
 for (( i=1; i<=60; i++ ))
 do
@@ -97,6 +105,7 @@ if [ "${convert_success}" = false ]; then
   echo -e "The image conversion to public failed after 600 seconds, please check image ${public_image_id} status"
   exit 1
 fi
+
 echo -e "Enable HVM mode for the public stemcell ${public_image_id}"
 hvm_enabled=`curl -sk https://$sl_username:${SL_API_KEY}@api.softlayer.com/rest/v3/SoftLayer_Virtual_Guest_Block_Device_Template_Group/${public_image_id}/setBootMode/HVM.json`
 if [ "${hvm_enabled}" != true ]; then
