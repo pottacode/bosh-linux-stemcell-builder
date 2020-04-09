d904a288d7641c38189d1dc80f394e2f2337589a
gu-bin <gubin@cn.ibm.com>
2020-04-09 11:45:44 +0800

    customize SR version and no need to upload certs as tls is disabled

--- ci/custom_stemcell/tasks/test-stemcell.sh
+++ ci/custom_stemcell/tasks/test-stemcell.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Login to bosh-lite"
+set -ex
+
 bosh2 envs
 echo "${CA_CERT}" >>~/.bosh/config 
 stemcell_version=$(cat version/number | sed 's/\.0$//;s/\.0$//')
@@ -13,6 +14,7 @@ bosh2 -e ${ALIAS} upload-release http://${filew3_server}/releases/security-relea
 if [ -e "deploy-yml/yml/custom-stemcell.yml" ]; then
   bosh2 int deploy-yml/yml/custom-stemcell.yml \
   -v stemcell_version=${stemcell_version} \
+  -v SR_version=${RELEASE} \
   --var-errs \
   > deploy-yml/yml/gen-custom-stemcell.yml
   bosh2 -e ${ALIAS} -d custom-stemcell deploy deploy-yml/yml/gen-custom-stemcell.yml -n
@@ -21,21 +23,21 @@ else
  exit 1
 fi
 
-bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/ca.pem" -d custom-stemcell 
-bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/client.crt" -d custom-stemcell 
-bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/client.key" -d custom-stemcell 
-bosh2 -e ${ALIAS} ssh -c "echo '${security_client_crt}' >/var/vcap/jobs/post-deploy/config/certs/logstash/client.crt" -d custom-stemcell >/dev/null 
-bosh2 -e ${ALIAS} ssh -c "echo '${security_client_key}' >/var/vcap/jobs/post-deploy/config/certs/logstash/client.key" -d custom-stemcell >/dev/null
-bosh2 -e ${ALIAS} ssh -c "echo '${security_ca_pem}' >/var/vcap/jobs/post-deploy/config/certs/logstash/ca.pem" -d custom-stemcell >/dev/null
-sleep 60
-vm_status=`bosh2 -e ${ALIAS} vms |grep "custom-stemcell" |awk -F ' ' '{print $2}'`
-
-if [ "${vm_status}" = "running" ]; then
-    echo "deploy vm success"
-else
-    echo "failed to deploy vm"
-    exit 1
-fi
+# bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/ca.pem" -d custom-stemcell
+# bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/client.crt" -d custom-stemcell
+# bosh2 -e ${ALIAS} ssh -c "sudo chmod 777 /var/vcap/jobs/post-deploy/config/certs/logstash/client.key" -d custom-stemcell
+# bosh2 -e ${ALIAS} ssh -c "echo '${security_client_crt}' >/var/vcap/jobs/post-deploy/config/certs/logstash/client.crt" -d custom-stemcell >/dev/null
+# bosh2 -e ${ALIAS} ssh -c "echo '${security_client_key}' >/var/vcap/jobs/post-deploy/config/certs/logstash/client.key" -d custom-stemcell >/dev/null
+# bosh2 -e ${ALIAS} ssh -c "echo '${security_ca_pem}' >/var/vcap/jobs/post-deploy/config/certs/logstash/ca.pem" -d custom-stemcell >/dev/null
+# sleep 60
+# vm_status=`bosh2 -e ${ALIAS} vms |grep "custom-stemcell" |awk -F ' ' '{print $2}'`
+#
+# if [ "${vm_status}" = "running" ]; then
+#     echo "deploy vm success"
+# else
+#     echo "failed to deploy vm"
+#     exit 1
+# fi
 
 private_ip=`bosh2 -e ${ALIAS} ssh -c "sudo ifconfig" -d custom-stemcell |grep 'inet addr' |head -1 |awk -F ':' '{print $3}'|awk -F ' ' '{print $1}'`
 echo "The new vm private ip is " $private_ip
