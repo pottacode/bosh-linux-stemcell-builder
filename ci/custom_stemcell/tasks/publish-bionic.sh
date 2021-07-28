6d30fdd28f66b1b614524774713322ad81739f80
donacarr <dcarriero@it.ibm.com>
2021-07-28 11:55:35 +0200

    fix S3 COS region in the URL

--- ci/custom_stemcell/tasks/publish-bionic.sh
+++ ci/custom_stemcell/tasks/publish-bionic.sh
@@ -6,7 +6,7 @@ VERSION=$(cat version/number | sed 's/\.0$//;s/\.0$//')
 mkdir -p bluemix-stemcell
 cp light-bluemix-stemcell/*.tgz bluemix-stemcell/
                                                                                     
-fileUrl=https://s3.us.cloud-object-storage.appdomain.cloud/${PUBLISHED_BUCKET_NAME}/light-bosh-stemcell-${VERSION}-bluemix-xen-ubuntu-bionic-go_agent.tgz
+fileUrl=https://s3.us-south.cloud-object-storage.appdomain.cloud/${PUBLISHED_BUCKET_NAME}/light-bosh-stemcell-${VERSION}-bluemix-xen-ubuntu-bionic-go_agent.tgz
 echo -e "Stemcell Download URL -> ${fileUrl}"
 checksum=`curl -L ${fileUrl} | sha1sum | cut -d " " -f 1`
 echo -e "Sha1 hashcode -> $checksum"
