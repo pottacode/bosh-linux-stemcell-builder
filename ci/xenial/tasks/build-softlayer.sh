6258120f4f4de7195be46a7b6ac01586c2fed013
donacarr <dcarriero@it.ibm.com>
2023-01-10 10:45:42 +0100

    bundle install needs root permission

--- ci/xenial/tasks/build-softlayer.sh
+++ ci/xenial/tasks/build-softlayer.sh
@@ -65,12 +65,13 @@ chown -R ubuntu:ubuntu bosh-linux-stemcell-builder
 
 sudo chmod u+s $(which sudo)
 
+pwd
+bundle install --local
+
 sudo --preserve-env --set-home --user ubuntu -- /bin/bash --login -i <<SUDO
   set -e
 
   cd bosh-linux-stemcell-builder
-  gem install bundler:2.22.2
-  bundle install --local
   bundle exec rake stemcell:build[$IAAS,esxi,$OS_NAME,$OS_VERSION,$CANDIDATE_BUILD_NUMBER]
   rm -f ./tmp/base_os_image.tgz
 SUDO
